fc0ed87d91285399f11fa4f2c835f86250a3cd72
regirip-debug <regirip@gmail.com>
2026-05-14 17:22:26 +0200

    Reorder 'À propos' link in navigation

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -4,8 +4,8 @@ const LINKS = [
   { route: "livre", href: "/", label: "Le livre" },
   { route: "recit", href: "/#extrait", label: "Le récit" },
   { route: "conferences", href: "/conferences", label: "Conférences" },
-  { route: "apropos", href: "/apropos", label: "À propos" },
   { route: "acheter", href: "/#acheter", label: "Tarifs" },
+  { route: "apropos", href: "/apropos", label: "À propos" },
   { route: "contact", href: "/contact", label: "Contact" },
 ];
 
